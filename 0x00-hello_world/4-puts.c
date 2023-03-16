8e63c7b81be9b4d2d5e40a7114ee0910f19419ea
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-16 12:09:11 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 */
 int main(void)
 {
-	char text[] = "Programming is like building a multilingual puzzle\n";
+	char text[] = "\"Programming is like building a multilingual puzzle\n";
 
 	/*writing to stdout*/
 	puts(text);
