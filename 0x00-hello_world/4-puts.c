2c6ca8efa2d0d9a100d965bf4139b16c2b01a4d4
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-16 11:41:37 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+*puts -entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char text[] = "Programming is like building a multilingual puzzle\n";
+
+	/*writing to stdout*/
+	puts(text);
+	return (0);
+}
