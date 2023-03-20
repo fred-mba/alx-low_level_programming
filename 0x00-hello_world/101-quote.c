0e2bbbb816cd08ce5f19b854fcc79287ade37d94
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-20 07:50:17 +0000

    Last fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+
 /**
 *main - Entry point
-*Return: Always return 0
+*Return: 1
 */
+
 int main(void)
 {
-	write(1, "and that piece of art is useful" '-' / 
-	';' Dora Korpar, 2015 '-' 10 '-' 19");
+	int s;
 
-	return (0);
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+
+	return (1);
 }
