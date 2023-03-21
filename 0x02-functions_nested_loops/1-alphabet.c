81a24f840a80fb51ce4c46e6183baf35dd1f7acd
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-21 22:37:13 +0000

    Fix-2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints all alphabets in lowercase
+*
+*/
+
+void print_alphabet(void)
+{
+	char lower_c;
+
+	for (lower_c = 'a'; lower_c <= 'z'; ++lower_c)
+	{
+		_putchar(lower_c);
+	}
+
+	_putchar('\n');
+}
