bfcc979a99c67fd2a43da12263c35dabcff2e0ea
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-21 23:37:33 +0000

    Fix-4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_islower - checks if the character is a lower case
+*@c: the character on test
+*
+*Return: return 1 for lowercase or 0 for uppercase
+*/
+
+int _islower(int c);
+{
+	if (c == 99)
+	{
+		return (1);
+	}
+	else if (c == 67)
+	{
+		return (0);
+	}
+}
