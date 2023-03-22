04dd0d2160668c55f8b969cc7f72bc51e5cd09b1
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-22 15:16:59 +0000

    Trial-11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,18 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		if (n < 98)
-		{
-			printf("%d, ", n);
-			++n;
-		}
-		else if (n == 98)
-		{
-			printf("%d\n");
-		}
-		else
-		{
-			printf("%d, ", n);
-			--n;
-		}
+		printf("%d, ", n);
+		++n;
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else
+	{
+		printf("%d, ", n);
+		--n;
 	}
 }
