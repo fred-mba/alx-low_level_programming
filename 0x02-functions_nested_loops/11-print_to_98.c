cbdc25f604c51ae552843e6766124808cf2ac212
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-22 16:09:55 +0000

    Fix-11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,36 @@
 
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
+	if (n <= 98)
 	{
-		if (n == 98)
+		/*letter i for my case means input number*/
+		/*n is the original assigned number*/
+		for (int i = n; i <= 98; ++i)
 		{
-			printf("%d\n" n);
+			if (i == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+			/*incase the input number is less than 98, increase the order*/
+			{
+				printf("%d\n, ", i);
+			}
 		}
-		else if (n <= 98)
+	}
+	/*Satisfy the condition if the number is greater than 98*/
+	else
+	{
+		for (int i = n; i >= 98; --i)
 		{
-			printf("%d, ", n);
-		}
-		for (n = 0; n >= 98; --n)
-		{
-			printf("%d, ", n);
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
 		}
 	}
 }
