2ba2b66e8cd1ac06665d63cb7d3f4ce56fd93eee
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-22 09:03:54 +0000

    Fix-9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#iclude "main.h"
+/**
+*times_table - prints times table starting with 0
+*
+*/
+
+void times_table(void)
+{
+	int column, row, number;
+
+	for (column = 0; column <= 9; column++)
+	{
+		for (row = 0; row <= 9; row++)
+		{
+			number = column * row;
+			if (row != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (number >= 10)
+			{
+				_putchar((number / 10) + '0');
+				_putchar((number % 10) + '0');
+			}
+			else if (number < 10 && row != 0)
+			{
+				_putchar(' ');
+				_putchar((numbers % 10) + '0');
+			}
+			else
+				_putchar((number % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
