fa68ac8064c0e58976c9079ed840432c8b42af62
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-22 13:56:10 +0000

    8th-fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-*jack_bauer - fucntion that prints every minute of the day of Jack Bauer, \
+*jack_bauer - prints every minute of the day of Jack Bauer, \
 *		starting from 00:00 to 23:59
 *
+*Return: return time
 */
 
 void jack_bauer(void)
