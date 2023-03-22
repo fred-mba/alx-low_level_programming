286dc5774a999b4fc6bd05eb314165c4c53ec746
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-22 23:01:36 +0000

    Fix-12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,6 @@ void print_times_table(int n)
 	for (column = 0; column <= n; column++)
 	{
 		printf("%2d", column);
-
 		for (multiplier = 0; multiplier <= n; multiplier++)
 		{
 			result = column * multiplier;
