cd302859265f8e20c939a438e7c8057f8ce66661
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-22 02:02:11 +0000

    11th_fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*add - sums up the two variables
+*
+*Description - 'adds two numbers'
+*
+*@n: 1st argument of a number
+*@m: 2nd argument number
+*Return: Always 0
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
