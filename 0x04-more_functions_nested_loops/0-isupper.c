6423420ce2d76753db69557ec2206901df024ecd
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-23 12:17:21 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,17 +4,14 @@
 *_isupper - checks for uppercase character
 *@c: character initialization
 *
-*Return: 'return 1 if c is upper and 0 for code success.'
+*Return: 'return 1 if c is upper, 0 otherwise
 */
 
 int _isupper(int c)
 {
-	if (c <= 90)
-	{
+	if (c >= 65 && c <= 90)
+
 		return (1);
-	}
 	else
-	{
-		return (0);
-	}
+		return  (0);
 }
