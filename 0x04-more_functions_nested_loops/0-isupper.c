39553a07d16e33a208fc62cb1fa9ebf339a725c1
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-23 10:53:57 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper - checks for uppercase character
+*@c: character initialization
+*
+*Return: return 1 if c is upper and 0 for code success.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 || c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
