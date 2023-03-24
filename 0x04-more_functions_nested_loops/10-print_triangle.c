0bbec8c497e934794f2ce1833ea34cf6bbd56485
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-24 00:39:22 +0000

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*print_triangle - prints a square using #
+*@size: number of # required to make a triangle
+*
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j >= 1; j--)
+			{
+				if (i < j)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
