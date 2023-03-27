aa41dc0dbdddfd2a26ce2f77847e28d0c4a9ba8b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-27 11:27:06 +0000

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reset_to_98 - updates the value of variable n to 98
+ *@n: a pointer the integer we want to set to 402
+ *
+ *Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
