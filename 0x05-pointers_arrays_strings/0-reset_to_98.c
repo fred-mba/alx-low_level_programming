66b424f047d657f33f24261c0c6d4385fabdcbfe
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-27 13:12:25 +0000

    Updates

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *reset_to_98 - updates the value of variable n to 98
- *@n: a pointer the integer we want to set to 402
+ *reset_to_98 - updates the value of variable n
+ *@n: a pointer
  *
  *Return: void
  */
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: Assign string to a pointer
+ *
+ *Return: return the string length
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		;
+	}
+	return (n);
+}
