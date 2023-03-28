4c2f294bf7cae0f73ef8f05361ae9221eb94fb19
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-28 20:53:37 +0000

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,17 +10,19 @@
 
 int main(void)
 {
-	int t, rand_num, gen_pasw;
+	int t, rand_num, max_range;
+
+	max_range = 2250;
 
 	srand(time(NULL));
 
-	for (t = 0; t <= 2250; t++)
+	for (t = 0; t <= max_range; t++)
 	{
 		rand_num = ((rand() % 125) + 1);
 
 		printf("%c", rand_num);
 
-		gen_pasw -= rand_num;
+		max_range -= rand_num;
 	}
 	printf("\n");
 
