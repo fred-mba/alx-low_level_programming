1dc6a5ee742886d9570828bbcdd9ab5b9eb03771
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-28 13:37:46 +0000

    Sacrifice numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -38,7 +38,11 @@ int _atoi(char *s)
 	/*processing the result*/
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		output = output * 10 + (s[i] - '0');
+		if (result < '0')
+
+			output = output * 10 + (s[i] - '0');
+		else
+			output = (s[i] - '0') * -1;
 		++i;
 	}
 	return (sign * output);
