38a83544003332e1c4a11c0fedcd6c7d83b0d416
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-29 13:41:59 +0000

    Strongcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenates two strings
+ *Description: it will use at most n bytes from src
+ *src does not need to be null-terminated if it
+ *	contains n or more bytes
+ *@src: string source
+ *@dest: destination buffer
+ *@n: input nth number
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = ('\0');
+	return (dest);
+}
