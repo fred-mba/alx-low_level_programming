99701da2f48b20c4dbe8da5ac6fb5bcf702ab71d
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-29 14:24:39 +0000

    Strongcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,9 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
+	/*move to the end of the destination string*/
+	/*i point to the terminating null character in dest*/
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
