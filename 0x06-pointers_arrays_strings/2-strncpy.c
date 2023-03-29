be0a874057a3aa0e06e7efeb873b664d83b4a174
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-03-29 19:48:30 +0000

    stringcopy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strncpy - copies string to destination
+ *Description: if the dest array is empty, return NULL
+ *		else, start from the beginning of dest
+ *@src: string source
+ *@dest: destination buffer
+ *@n: index number
+ *Return: the copied string in dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr;
+
+	ptr = dest; /*pointing beginning of destination buffer*/
+	while (src && n--) /*copy characters in src into array pointed by dest*/
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	dest = ('\0'); /*when encounter null in src, terminate*/
+	return (ptr);
+}
