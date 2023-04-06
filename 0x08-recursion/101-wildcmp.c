9b4a4fce4b083b5244d3793456315ee56586090a
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-06 14:42:51 +0000

    9-recursive

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,13 +12,13 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == *s2)
-		return (wildcmp(++s1, ++s2));
+		return (wildcmp(s1 + 1, s2 + 1));
 	/*If there is wildcard in s2, move to the next character and compare*/
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, ++s2));
-		if (wildcmp(++s1, s2) || wildcmp(s1, ++s2))
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
 	}
 	return (0);
