0589c503a4d4cafc009cb11b522325d574f0dd18
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-08 11:35:38 +0000

    2-argc/argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - prints all arguments it receives.
+ *@argc: argument count
+ *@argv: one-dimensional string array
+ *Return: 0 ==> success
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+
+		printf("%s\n", *argv++);
+	return (0);
+}
