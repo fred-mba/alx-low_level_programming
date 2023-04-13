d5e919a7cb02188955b0879e3af74c47984c6203
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-13 17:46:47 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *a;
-	unsigned int i, res;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,8 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (a == NULL)
 		return (NULL);
 
-	res = size * nmemb;
-	for (i = 0; i > res; i++)
+	for (i = 0; i > nmemb; i++)
 	{
 		*a = 0; /*set memory to zero*/
 	}
