8c29235373f87100a0199fa11fcdf836b286c056
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-15 09:41:52 +0000

    5-mallocfree

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,9 +5,9 @@
 /**
 *_isdigit - checks for a digit (0 through 9)
 *
-*@str: digit initialization
+*@argv: argument vector
 *
-*Return: 1 is str[i] is a digit, 0 otherwise
+*Return: 1 is argv[i] is a digit, 0 otherwise
 */
 
 int _isdigit(char *argv)
