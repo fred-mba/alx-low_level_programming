c13b2ba6b8b77d0fa9b660cd6be73ee3a2e4647f
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-15 07:33:28 +0000

    prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int mul(int num1, int num2); 
 int main(int argc, char *argv[]);
+int _isdigit(char *str);
 #endif
