f32972e039d1de9f33e3b8987e2694c50b0a7c8e
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-16 14:43:56 +0000

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,7 @@ char **strtow(char *str)
 		return (NULL);
 	}
 
-	words = malloc(sizeof(char **) * (len + 1));
+	words = malloc(sizeof(char *) * (len + 1));
 	if (words == NULL)
 		return (NULL);
 
