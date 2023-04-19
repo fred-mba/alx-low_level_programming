c3d36bc3014a7975f68984392c0d7b462a867b57
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-19 23:15:04 +0000

    main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,6 +21,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	operator = get_op_func(argv[2]);
 
 	if (operator == NULL) /*operator is none of the above*/
