77fed2e36b2e83fc893a77e291f3d31fe5f9a0f2
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-21 04:32:23 +0000

    3-variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,48 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_char - print char type element passed to va_list
- * @list: argument passed to function
+ * @c: argument passed to function
  */
-void print_char(va_list list)
+void print_char(va_list c)
 {
-	printf("%c", va_arg(list, int));
+	printf("%c", va_arg(c, int));
 }
 
 /**
  * print_str - print string from va_list
- * @list: string passed to function
+ * @s: string passed to function
  */
-void print_str(va_list list)
+void print_str(va_list s)
 {
 	char *p;
 
-	p = va_arg(list, char *);
+	p = va_arg(s, char *);
 	if (p == NULL)
-		p = "(nil)";
-	printf("%s", p);
+	{
+		printf("(nil)");
+	}
+	else
+		printf("%s", p);
 }
 
 /**
  * print_float - print float type from va_list
- * @list: double passed to function
+ * @f: double passed to function
  */
-void print_float(va_list list)
+void print_float(va_list f)
 {
-	printf("%f", va_arg(list, double));
+	printf("%f", va_arg(f, double));
 }
 
 /**
  * print_int - print int type element from va_list
- * @list: integer passed to function
+ * @itn: integer passed to function
  */
-void print_int(va_list list)
+void print_int(va_list tin)
 {
-	printf("%d", va_arg(list, int));
+	printf("%d", va_arg(tin, int));
 }
 
 /**
@@ -51,27 +54,27 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
 	va_list ap;
-	char *sep;
+	char *separator;
 
-	specifiers[] = {
-		{ "c", _printchar },
-		{ "f", _printfloat },
-		{ "s", _printstr },
-		{ "i", _printint }
+	specifier list[] = {
+		{ "c", print_char },
+		{ "f", print_float },
+		{ "s", print_str },
+		{ "i", print_int }
 	};
 
 	i = 0;
-	sep = "";
+	separator = "";
 	va_start(ap, format);
-	while (format && format[i])
+	while (format != NULL && format[i])
 	{
 		j = 0;
 		while (j < 4)
 		{
-			if (format[i] == *valid_types[j].valid)
+			if (format[i] == *list[j].type)
 			{
 				printf("%s", separator);
-				valid_types[j].f(ap);
+				list[j].f(ap);
 				separator = ", ";
 			}
 			j++;
