4a404266cb76c39d41b14d9729f53ec53cc21d17
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-22 13:17:15 +0000

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NB_BYTES 40
-
 /**
  *main - prints the opcodes of its own main function
  *@argc: argument count
@@ -22,8 +20,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	num_bytes = NB_BYTES;
-
 	num_bytes = atoi(argv[1]);
 
 	/*check if number of bytes is negative*/
@@ -38,8 +34,8 @@ int main(int argc, char *argv[])
 		print_opcode = *(unsigned char *)ptr;
 		printf("%02x ", print_opcode);
 
-		if (index == num_bytes - 1)
-			continue;
+		if (index < num_bytes - 1)
+			printf(" ");
 
 		ptr++;
 	}
