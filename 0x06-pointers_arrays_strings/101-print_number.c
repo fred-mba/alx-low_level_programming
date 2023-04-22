8b9e8e40cf9c8159128ec0b287cee00ba0ee6fcd
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-22 14:17:02 +0000

    Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_number - function that prints an integer.
+ *@n: number
+ *
+ *Return: 0 ==> success
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10 != '\0')
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
