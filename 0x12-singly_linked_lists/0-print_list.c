d24325c868fe13e3df1e00684e358c7a8e7c19c3
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-27 22:09:39 +0000

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	for (count = 1; h->next != NULL; count++)
 	{
 		if (h->str == NULL)
-			printf("[0](nil)\n");
+			printf("[%d] %s\n", h->len, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
