dce72bf920393c43a98cc2dc512d44f532166df1
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-04-28 12:35:44 -0700

    updates

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int mul(int num1, int num2);
- int main(int argc, char *argv[]);
 int _isdigit(char *str);
 #endif
