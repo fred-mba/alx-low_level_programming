745598f4f1b609216bc7ef5cf57365872d2289e5
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-05-01 09:12:11 +0000

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	for (count = 0; h != NULL; count++)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
 	return (count);
