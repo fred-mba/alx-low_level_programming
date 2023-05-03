4c208d0a46fb94252e6bf616877e67f7fa71695c
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-05-03 19:51:41 +0000

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,21 +9,61 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *bin;
-	size_t i = 0;
+	listint_t **list = NULL;
+	size_t size = 0;
+	listint_t *current, *next;
+	size_t i;
+
+	if (h == NULL)
+		return (0);
 
 	current = *h;
-	while (current != NULL)
+	while (current)
 	{
-		i++;
-		bin = current;
-		current = current->next;
-		free(bin);
-
-		if (bin < current)
-			break;
+		next = current->next;
+		for (i = 0; i < size; i++)
+		{
+			if (current == list[i])
+			{
+				free(list);
+				*h = NULL;
+				return (size);
+			}
+		}
+		list = _ra(list, size + 1, current);
+		size++;
+		free(current);
+		current = next;
 	}
+	free(list);
 	*h = NULL;
+	return (size);
+}
 
-	return (i);
+/**
+ *_ra - reallocates mem for an array of pointers
+ *@list: the old list to append
+ *@size: size of the new list
+ *@new: new node to add to the list
+ *Return: pointer to the new list
+ */
+
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		newlist[i] = list[i];
+	}
+	newlist[i] = new;
+	free(list);
+	return (newlist);
 }
