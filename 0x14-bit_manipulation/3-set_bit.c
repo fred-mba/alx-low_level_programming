cad579c765ae52cac8a0771ee073c1e428eedf42
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-05-04 21:40:49 +0000

    3-bitwise

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (8 * sizeof(*n) - 1))
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	*n |= (1 << index);
 	return (1);
