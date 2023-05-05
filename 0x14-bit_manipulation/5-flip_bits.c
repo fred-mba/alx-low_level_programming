f4664ac62774d0cc4bc76f98c9bdefa6d4281daf
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-05-05 01:45:15 +0000

    5-bitwise

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *flip_bits - returns the number of bits needed to flip
+ *	to get from one number to another
+ *@n: 1st operand number
+ *@m: 2nd operand number
+ *Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned int XOR = n ^ m;
+
+	while (XOR)
+	{
+		count += XOR & 1;
+		XOR >>= 1;
+	}
+	return (count);
+}
