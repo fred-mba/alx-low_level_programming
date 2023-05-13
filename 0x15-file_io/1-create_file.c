2bf288aaeb4ae5bdbbff1d6e73278bdac8399b2d
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-05-13 14:10:02 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fclose(new_file);
-	chmod(filename, S_IRUSR | S_IWUSR);
+
+	if (access(filename, F_OK) == 0)
+		chmod(filename, S_IRUSR | S_IWUSR);
 
 	return (1); /*created successfully*/
 }
