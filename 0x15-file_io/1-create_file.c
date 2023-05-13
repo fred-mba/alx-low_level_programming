ea702231f979e8e153c4a57681988ea75834a833
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-05-13 13:38:43 +0000

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *create_file - creates a file and writes text_content in a file
+ *@text_content: text to be written on a new file
+ *@filename: pointer to the new file
+ *The created file must have rw------- permissions
+ *If text_content is NULL create an empty file
+ *Return: 1 on success, -1 if file can not be created, file can not
+ *	be written, write “fails”
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *new_file;
+	size_t text_len = strlen(text_content);
+	size_t bytes_written;
+
+	new_file = fopen(filename, "w");
+	if (new_file == NULL)
+	{
+		return (-1); /*if file fails to open*/
+	}
+
+	if (text_len > 0)
+	{
+		bytes_written = fwrite(text_content, sizeof(char), text_len, new_file);
+		if (bytes_written < text_len)
+		{
+			fclose(new_file); /*write fails*/
+			return (-1);
+		}
+	}
+
+	fclose(new_file);
+	chmod(filename, S_IRUSR | S_IWUSR);
+
+	return (1); /*created successfully*/
+}
