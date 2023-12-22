fda8bde50aeead8f897061b5263230ebde13ae71
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2023-12-22 22:34:12 +0300

    Print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ *@ht: hash table pointer
+ *Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char tmp = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (tmp == 1)
+				printf(", ");
+			printf("'%s' : '%s'", node->key, node->value);
+			tmp = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
